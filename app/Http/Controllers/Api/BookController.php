7dffb6acd14c8578ec24bdab05dd78eb89baa1d6
daichi.sugiyama <caad12.di2@gmail.com>
2023-04-26 12:48:46 +0900

    #10 全書籍取得のルーティング、コントローラを設定

---
+++ app/Http/Controllers/Api/BookController.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Core\src\Book\UseCase\GetAllBooks\GetAllBooks;
+
+class BookController extends Controller
+{
+    /**
+     * 全書籍レコード取得
+     */
+    public function index(GetAllBooks $useCase)
+    {
+        return response()->json([
+            'allBooks' => $useCase->execute(),
+        ]);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(string $id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, string $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(string $id)
+    {
+        //
+    }
+}
