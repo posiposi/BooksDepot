0b91816bc9f866030e3758161bed192e456e99bf
daichi.sugiyama <caad12.di2@gmail.com>
2023-04-27 11:09:49 +0900

    #15 共通メニューコンポーネント作成

---
+++ resources/js/components/CommonMenu.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <!-- 上部メニュー -->
+    <v-app-bar color="primary">
+      <v-app-bar-nav-icon @click="toggleSidebarDisplay()"></v-app-bar-nav-icon>
+      <v-toolbar-title>BooksDepot</v-toolbar-title>
+    </v-app-bar>
+    <!-- サイドバー -->
+    <v-navigation-drawer v-model="drawer">
+      <v-list nav>
+        <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
+        <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
+        <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+// サイドバー表示切り替え
+const drawer = ref<boolean>(false);
+
+/**
+ * サイドバーの表示を切り替える
+ */
+const toggleSidebarDisplay = (): void => {
+  drawer.value = !drawer.value;
+}
+</script>
