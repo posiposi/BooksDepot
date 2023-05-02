34722fdf2cbdaffb68266852e347e07026216dd9
daichi.sugiyama <caad12.di2@gmail.com>
2023-05-02 14:12:23 +0900

    #14 書籍の型指定ファイルを作成

---
+++ resources/js/types/book.d.ts
@@ -0,0 +1,12 @@
+export declare type Book = {
+	id: number;
+	title: string;
+	year_of_publication: string;
+	rating: number;
+	author_id: number;
+	publisher_id: number;
+	impression_id: number;
+	created_at: string;
+	updated_at: string;
+	deleted_at: string | null;
+};
