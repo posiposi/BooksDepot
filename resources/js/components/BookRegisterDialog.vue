040982157877e915c454f1879bda86ee182f5277
daichi.sugiyama <caad12.di2@gmail.com>
2023-05-21 10:43:56 +0900

    #26 書籍登録ダイアログコンポーネントを実装

---
+++ resources/js/components/BookRegisterDialog.vue
@@ -0,0 +1,20 @@
+<template>
+  <v-card>
+    <v-card-text>
+      ここに書籍登録フォームのテキストを入力する。
+    </v-card-text>
+    <v-card-actions>
+      <v-btn color="primary" block @click="submit">登録</v-btn>
+    </v-card-actions>
+  </v-card>
+</template>
+
+<script setup lang="ts">
+const emit = defineEmits(['clickSubmit']);
+/**
+ * 親コンポーネントに登録イベントを通知する
+ */
+const submit = () => {
+  emit('clickSubmit');
+}
+</script>
