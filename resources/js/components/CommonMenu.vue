198411d0b22bc86e60558528be19b88f06de6a11
daichi.sugiyama <caad12.di2@gmail.com>
2023-05-21 10:43:36 +0900

    #26 書籍登録ダイアログ表示ボタンをナビゲーションバーに設定

--- resources/js/components/CommonMenu.vue
+++ resources/js/components/CommonMenu.vue
@@ -4,28 +4,44 @@
     <v-app-bar color="primary">
       <v-app-bar-nav-icon @click="toggleSidebarDisplay()"></v-app-bar-nav-icon>
       <v-toolbar-title>BooksDepot</v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn variant="outlined" @click="toggleRegisterDialogDisplay()">登録</v-btn>
     </v-app-bar>
     <!-- サイドバー -->
-    <v-navigation-drawer v-model="drawer">
+    <v-navigation-drawer v-model="isDrawer">
       <v-list nav>
         <v-list-item prepend-icon="mdi-email" title="Inbox" value="inbox"></v-list-item>
         <v-list-item prepend-icon="mdi-account-supervisor-circle" title="Supervisors" value="supervisors"></v-list-item>
         <v-list-item prepend-icon="mdi-clock-start" title="Clock-in" value="clockin"></v-list-item>
       </v-list>
     </v-navigation-drawer>
+    <!-- 書籍登録ダイアログ -->
+    <v-dialog v-model="isRegisterDialog" width="auto">
+      <book-register-dialog @clickSubmit="toggleRegisterDialogDisplay()"></book-register-dialog>
+    </v-dialog>
   </div>
 </template>
 
 <script setup lang="ts">
 import { ref } from "vue";
+import BookRegisterDialog from './BookRegisterDialog.vue';
 
-// サイドバー表示切り替え
-const drawer = ref<boolean>(false);
+// サイドバー表示
+const isDrawer = ref<boolean>(false);
+// 書籍登録ダイアログ表示
+const isRegisterDialog = ref<boolean>(false);
 
 /**
  * サイドバーの表示を切り替える
  */
 const toggleSidebarDisplay = (): void => {
-  drawer.value = !drawer.value;
+  isDrawer.value = !isDrawer.value;
+}
+
+/**
+ * 書籍登録ダイアログの表示を切り替える
+ */
+const toggleRegisterDialogDisplay = (): void => {
+  isRegisterDialog.value = !isRegisterDialog.value;
 }
 </script>
