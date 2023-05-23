469166d7dca17a516d8bce5cfc342cb20d1c3b78
daichi.sugiyama <caad12.di2@gmail.com>
2023-05-23 20:19:17 +0900

    #26 不要なpropsを削除

--- resources/js/components/BookRegisterDialog.vue
+++ resources/js/components/BookRegisterDialog.vue
@@ -8,7 +8,7 @@
             <v-text-field v-model="registerData.title.value" label="タイトル" required></v-text-field>
           </v-col>
           <v-col cols="12" md="4">
-            <v-text-field v-model="registerData.year_of_publication.value" label="出版年" required></v-text-field>
+            <v-text-field v-model="registerData.author_id.value" label="著者" required></v-text-field>
           </v-col>
           <v-col cols="12" md="4">
             <v-text-field v-model="registerData.rating.value" label="おすすめ度" required></v-text-field>
@@ -16,13 +16,13 @@
         </v-row>
         <v-row>
           <v-col cols="12" md="4">
-            <v-text-field v-model="registerData.author_id.value" label="著者" required></v-text-field>
-          </v-col>
-          <v-col cols="12" md="4">
             <v-text-field v-model="registerData.publisher_id.value" label="出版社" required></v-text-field>
           </v-col>
+          <v-col cols="12" md="4">
+            <v-text-field v-model="registerData.year_of_publication.value" label="出版年" required suffix="年"></v-text-field>
+          </v-col>
         </v-row>
-        <v-row justify="end">
+        <v-row justify="center">
           <v-btn color="primary" @click="submit">登録</v-btn>
         </v-row>
       </v-container>
@@ -33,21 +33,13 @@
 <script setup lang="ts">
 import { ref } from "vue";
 
-const date = ref<string>(new Date().toISOString().slice(0, 19).replace('T', ' '));
-
 const registerData = {
-  // TODO idはdefinePropsを使用して親から取得する(現状の最大id+1)
-  id: ref<number>(),
   title: ref<string>(''),
-  year_of_publication: ref<string>(''),
-  rating: ref<number>(),
   author_id: ref<number>(),
+  rating: ref<number>(),
   publisher_id: ref<number>(),
-  impression_id: ref<number>(),
-  // 下記はサーバ側処理も検討
-  created_at: date,
-  updated_at: date,
-  deleted_at: ref<string | null>(null),
+  year_of_publication: ref<string>(''),
+  impression_id: ref<number | null>(null),
 }
 
 const emit = defineEmits(['clickSubmit']);
